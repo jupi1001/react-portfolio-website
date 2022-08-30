e88d1c59844ccd2def9992398533d9071b1e0b5b
Laurids Hieden <realjupi1001@gmail.com>
2022-08-30 21:52:13 +0200

    Add Navbar +Header + About

---
+++ src/container/About/About.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import "./About.scss";
+import { images } from "../../constants";
+
+const abouts = [
+  { title: "Web development", description: "I am a good web developer", imgUrl: images.about01 },
+  { title: "Web Design", description: "I am a good web designer", imgUrl: images.about02 },
+  { title: "UI/UX", description: "I am good at ui/ux", imgUrl: images.about03 },
+  { title: "Java", description: "I have multiple years of experience", imgUrl: images.about04 },
+];
+
+const About = () => {
+  return (
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Design</span>
+        <br /> means <span>Good Business</span>
+      </h2>
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
