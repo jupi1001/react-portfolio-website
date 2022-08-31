f7bc8e2aef65b2aebd3b9202ebbd1fd5e703f76a
Laurids Hieden <realjupi1001@gmail.com>
2022-08-31 15:58:55 +0200

    Update env

--- src/client.js
+++ src/client.js
@@ -1,9 +1,10 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
+console.log("check token", process.env.REACT_APP_SANITY_PROJECT_ID);
+
 export const client = sanityClient({
-  // projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  projectId: "c5qkonoa",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
